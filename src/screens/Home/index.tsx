c9e7a745a2bf9bee45bd410288bcbec6b84b0907
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-06-22 15:31:28 -0300

    feat: add credentials listing and filter

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,43 +1,54 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useState, useCallback, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
-import { SearchBar } from '../../components/SearchBar';
-import { LoginDataItem } from '../../components/LoginDataItem';
+import { SearchBar } from "../../components/SearchBar";
+import { LoginDataItem } from "../../components/LoginDataItem";
 
 import {
   Container,
   LoginList,
   EmptyListContainer,
-  EmptyListMessage
-} from './styles';
+  EmptyListMessage,
+} from "./styles";
 
 interface LoginDataProps {
   id: string;
   title: string;
   email: string;
   password: string;
-};
+}
 
 type LoginListDataProps = LoginDataProps[];
 
 export function Home() {
-  // const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
-  // const [data, setData] = useState<LoginListDataProps>([]);
+  const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
+  const [data, setData] = useState<LoginListDataProps>([]);
 
   async function loadData() {
-    // Get asyncStorage data, use setSearchListData and setData
+    const storagedData = await AsyncStorage.getItem("@passmanager:logins");
+
+    const data = storagedData ? JSON.parse(storagedData) : [];
+
+    setData(data);
+    setSearchListData(data);
   }
   useEffect(() => {
     loadData();
   }, []);
 
-  useFocusEffect(useCallback(() => {
-    loadData();
-  }, []));
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [])
+  );
 
   function handleFilterLoginData(search: string) {
-    // Filter results inside data, save with setSearchListData
+    const filteredData = data.filter((e) =>
+      e.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setSearchListData(filteredData);
   }
 
   return (
@@ -50,19 +61,21 @@ export function Home() {
       <LoginList
         keyExtractor={(item) => item.id}
         data={searchListData}
-        ListEmptyComponent={(
+        ListEmptyComponent={
           <EmptyListContainer>
             <EmptyListMessage>Nenhum item a ser mostrado</EmptyListMessage>
           </EmptyListContainer>
-        )}
+        }
         renderItem={({ item: loginData }) => {
-          return <LoginDataItem
-            title={loginData.title}
-            email={loginData.email}
-            password={loginData.password}
-          />
+          return (
+            <LoginDataItem
+              title={loginData.title}
+              email={loginData.email}
+              password={loginData.password}
+            />
+          );
         }}
       />
     </Container>
-  )
+  );
 }
